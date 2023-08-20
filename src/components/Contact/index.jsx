8a1ad2708009569d057eac37654183f82a554dc7
kartikjain-sudo <kartikjain42@protonmail.com>
2023-08-20 17:06:35 +0530

    feat: adding contact me

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,91 @@
+
+import React, { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
+import './index.scss';
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
+
+function Contact() {
+
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeoutId); // Clear the timeout if the component unmounts
+    };
+  }, []);
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        refForm.current,
+        process.env.REACT_APP_PUBLIC_KEY,
+      ).then(() => {
+        alert("Email sent Successfully!")
+        window.location.reload(false)
+      }, () => {
+        alert("Failed to sent, Please Try Again.")
+      })
+  }
+
+  return (
+    <>
+    <div className='container contact-page'>
+      <div className='text-zone'>
+        <h1>
+          <AnimatedLetters letterClass={letterClass}
+          strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']} idx={15}/>
+        </h1>
+        <p>
+          Ut rhoncus rhoncus viverra. Ut id consectetur tortor. Morbi sagittis,
+          lectus ac varius feugiat, mauris elit cursus libero, et malesuada dui
+          turpis ac sem. Aliquam at augue non sapien pulvinar blandit id eget
+          nibh. Phasellus blandit dolor nec pretium euismod. Quisque id quam
+          eget risus accumsan mollis. Aliquam lobortis id nisi ac iaculis.
+          Aenean ultrices urna neque, luctus finibus nisl efficitur sed. Sed
+          mollis dapibus turpis id tincidunt.
+        </p>
+        <div className='contact-form'>
+          <form ref={refForm} onSubmit={sendEmail}>
+            <ul>
+              <li className='half'>
+                <input type='text' name='name' placeholder='Name' required/>
+              </li>
+              <li className='half'>
+                <input type='email' name='email' placeholder='Email' required/>
+              </li>
+              <li>
+                <input placeholder='Subject' name='subject' type='text' required />
+              </li>
+              <li>
+                <textarea placeholder='Message' name='message' required />
+              </li>
+              <li>
+                <input type='submit' className='flat-button' value='SEND' />
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+      <div className='info-map'>
+        Kartik Jain <br />
+        India <br />
+        <span>kartikjain42@protonmail.com </span>
+      </div>
+    </div>
+    <Loader type='pacman' />
+    </>
+  )
+}
+
+export default Contact
